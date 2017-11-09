76db14529adfe0ff5393941d5b132217817337b9
Dorision <ndorision@gmail.com>
2017-11-09 14:48:09 +0200

    profile finished

--- app/src/main/java/com/example/codetribe/palliate/AboutActivity.java
+++ app/src/main/java/com/example/codetribe/palliate/AboutActivity.java
@@ -1,7 +1,9 @@
 package com.example.codetribe.palliate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+    }
+
+
+    public void FAQ(View view) {
+        startActivity(new Intent(this, FaqActivity.class));
+
+    }
+
+    public void Contact(View view) {
+        startActivity(new Intent(this, Contact.class));
+
+    }
+
+    public void App(View view) {
+        startActivity(new Intent(this, AppActivity.class));
+
+
+
     }
 }
