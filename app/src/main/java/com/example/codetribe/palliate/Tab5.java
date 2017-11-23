f2a2b1c1d6e0a1b3e5f936d83ef2d0b618ba7919
swjiyane <swjiyane@gmail.com>
2017-11-23 09:47:17 +0200

    my updated version

---
+++ app/src/main/java/com/example/codetribe/palliate/Tab5.java
@@ -0,0 +1,49 @@
+package com.example.codetribe.palliate;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by CodeTribe on 11/9/2017.
+ */
+
+public class Tab5 extends android.support.v4.app.Fragment {
+
+    Context context;
+    TextView description;
+    TextView section_label;
+    ImageView image;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        context = getContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_pager,container,false);
+        //topic
+        section_label = rootView.findViewById(R.id.section_label);
+        section_label.setText(R.string.topic_five);
+
+        //image
+        image = rootView.findViewById(R.id.image_topic);
+        image.setImageResource(R.drawable.palliate_logo);
+
+        // description
+        description = rootView.findViewById(R.id.description);
+        description.setText(R.string.five_description);
+
+
+        return rootView;
+    }
+}
